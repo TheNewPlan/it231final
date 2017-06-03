42cba594969542b43cea9515160ce218f7077c5e
Matt Hallowell <matthewhallowell1@gmail.com>
2017-06-03 00:50:07 -0500

    Fixed routing for dead-ends as well as for user path navagation.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
   root :to => 'home#index'
+
   get 'home/index'
   get 'home/login'
   get 'home/logout'
+  get 'home/user'
+  get 'home/saveList'
+  get 'home/removeFinales'
 
   post 'home/login'
   post 'home/index'
+  post 'home/user'
+  post 'home/saveList'
+  post 'home/removeFinales'
+
 
   get 'search/search'
   post 'search/search'
@@ -20,12 +28,6 @@ Rails.application.routes.draw do
 
   get 'browse/search'
   get 'browse/sort'
-
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
-  get "login" => "sessions#new", :as => "login"
-  delete 'logout' => "sessions#destroy", :as => "logout"
 
   resources :sessions
   resources :finales
